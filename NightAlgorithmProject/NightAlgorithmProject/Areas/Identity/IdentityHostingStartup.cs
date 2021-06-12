42828ee68cc327d52a2e404fab87ed47a5ccfe7d
amnatrcalo <atrcalo1@etf.unsa.ba>
2021-06-12 17:01:37 +0200

    Dodane uloge u bazu i novi hmtl elementi

---
+++ NightAlgorithmProject/NightAlgorithmProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NightAlgorithmProject.Data;
+
+[assembly: HostingStartup(typeof(NightAlgorithmProject.Areas.Identity.IdentityHostingStartup))]
+namespace NightAlgorithmProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
