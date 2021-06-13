0a5128460cc7480a93eb57722f6f4f66d6d78bec
amnatrcalo <atrcalo1@etf.unsa.ba>
2021-06-13 11:47:44 +0200

    Urađena stranica o objektu

---
+++ NightAlgorithmProject/NightAlgorithmProject/Data/Migrations/20210613082506_danas.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NightAlgorithmProject.Data.Migrations
+{
+    public partial class danas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tipDogađaja",
+                table: "Dogadjaj",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "opisDogađaja",
+                table: "Dogadjaj",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "tipDogađaja",
+                table: "Dogadjaj",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "opisDogađaja",
+                table: "Dogadjaj",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
